f1f7fb788e0a8f31772ec3a185954c9722e843c4
CHRehan <chrehansarwar@gmail.com>
2022-04-22 14:41:25 +0500

    CI Pipeline testing

--- app/src/main/java/com/ci/cd/demo/MainActivity.kt
+++ app/src/main/java/com/ci/cd/demo/MainActivity.kt
@@ -22,7 +22,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    Greeting("Imported Govt")
                 }
             }
         }
@@ -38,6 +38,6 @@ fun Greeting(name: String) {
 @Composable
 fun DefaultPreview() {
     CICDDemoTheme {
-        Greeting("Android")
+        Greeting("Imported Govt")
     }
 }
